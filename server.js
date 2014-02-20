e3bdeebb45f9b8ae5f2a8a70a8de54f2d9ebd951
Nathan Woodbury <nathanswoodbury@gmail.com>
2014-02-20 15:13:05 -0700

    Added the ability for express to connect in server.js

--- server.js
+++ server.js
@@ -2,6 +2,10 @@
 //  Module Dependencies
 //=============================================================================
 
+var express = require('express');
+
+require('colors');
+
 //=============================================================================
 //  Main Application Entry (order is important)
 //=============================================================================
@@ -12,4 +16,24 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 // Initializing system variables
 var config = require('./config/config')(process.env.NODE_ENV);
 
-console.log(config);
+module.exports.start = function(done) {
+    var app = express();
+
+    app.listen(config.port, function() {
+        console.log(('App started, listening on port ' +
+                     config.port).green);
+
+        if (done) {
+            return done(null, app);
+        }
+    }).on('error', function(e) {
+        if (e.code === 'EADDRINUSE') {
+            console.log('Address in use. Is the server already running?'.red);
+        }
+        if (done) {
+            return done(e);
+        }
+    });
+};
+
+module.exports.start();
