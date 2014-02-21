8ab8524bacc035a1a4226a43dff1d700aa48d8ca
Nathan Woodbury <nathanswoodbury@gmail.com>
2014-02-20 17:56:36 -0700

    Added express configuration settings.

--- server.js
+++ server.js
@@ -7,21 +7,26 @@ var express = require('express'),
 
 require('colors');
 
+console.log(('\n\n[tdf] Starting app...').cyan);
+
 //=============================================================================
-//  Main Application Entry (order is important)
+//  Settings and Configurations
 //=============================================================================
 
 // Load Configurations (set default variables if not passed in the process)
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 process.env.PORT = process.env.PORT || 3000;
 
+
+//=============================================================================
 // Start up the app
+//=============================================================================
+
 Q.nfcall(require('./config/config'), process.env.NODE_ENV, process.env.PORT)
 .then(function(config) {
-    var app = express();
     var deferred = Q.defer();
-
-    console.log(('\n\n[tdf] Starting app...').cyan);
+    var app = express();
+    require('./config/express')(app, config/*, passport, db*/);
 
     app.listen(config.port, function() {
         deferred.resolve(config.port);
