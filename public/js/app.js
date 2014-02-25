f483dc0ee2ad8738a55a626f3d6477932b4fc80d
Nathan Woodbury <nathanswoodbury@gmail.com>
2014-02-24 17:13:15 -0700

    Configured front-end angular. Added header controller.

---
+++ public/js/app.js
@@ -0,0 +1,5 @@
+angular.module('tdf', ['ngCookies', 'ngResource',
+                       'ui.bootstrap', 'ui.router',
+                       'tdf.system']);
+
+angular.module('tdf.system', []);
---
+++ public/js/controllers/header.js
@@ -0,0 +1,50 @@
+angular.module('tdf.system').controller('HeaderController',
+    ['$scope', 'Global',
+    function ($scope, Global) {
+        $scope.global = Global;
+
+        $scope.setMenu = function() {
+            if (!$scope.global.user) {
+                $scope.menu = $scope.getPublicMenu();
+            }
+            else if ($scope.global.user.isAdmin) {
+                $scope.menu = $scope.getAdminMenu();
+            }
+            else {
+                $scope.menu = $scope.getPrivateMenu();
+            }
+        };
+
+        $scope.getPublicMenu = function() {
+            return [
+                {
+                    'title': 'Leagues',
+                    'link': 'leagues/'
+                }
+
+            ];
+        };
+
+        $scope.getAdminMenu = function() {
+            //var public_menu = $scope.getPublicMenu();
+            var private_menu = $scope.getPrivateMenu();
+            var admin_menu = [
+                {
+                    'title': 'All Users',
+                    'link': 'users'
+                }
+            ];
+            return private_menu.concat(admin_menu);
+        };
+
+        $scope.getPrivateMenu = function() {
+            var public_menu = $scope.getPublicMenu();
+            var private_menu = [
+                {
+                    'title': 'My Agents',
+                    'link': 'users/' + $scope.global.user._id
+                }
+            ];
+            return public_menu.concat(private_menu);
+        };
+    }]);
